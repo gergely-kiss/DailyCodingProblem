610c4f4219600451a20e35f929e3adb93fa26821
gergely <kiss-gergely@outlook.com>
2019-04-07 11:42:23 +0100

    init commit

---
+++ return-merged-list/src/main/java/uk/gergely/kiss/App.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
